2da0f5e61555f35ca3dfe27dcfe644bdf9992bad
Brian_Izaki <bkizaki@outlook.com>
2020-07-11 12:56:14 -0300

    add GederButton no NovoUsuario / att dir css no GenderButton

--- src/components/NovoUsuario/index.jsx
+++ src/components/NovoUsuario/index.jsx
@@ -1,58 +1,73 @@
-import React from 'react';
-import Label from '../Label';
-import Input from '../Input';
+import React from "react";
+import Label from "../Label";
+import Input from "../Input";
+import GenderSelector from "../GenderSelector";
 
-
-class NovoUsuario extends React.Component{
-  constructor(props){
+class NovoUsuario extends React.Component {
+  constructor(props) {
     super(props);
-    
+
     this.state = {
-      
       usuario: {
-        nome: '',
+        nome: "",
+        genero: "m",
       },
 
       validacao: {
         nomeInvalido: true,
+        generoInvalido: false,
       },
-
-    }
+    };
   }
-  
-  atualizarNome(e){
+
+  atualizarNome(e) {
     let usuario = this.state.usuario;
     usuario.nome = e.target.value;
     this.setState({
       usuario: usuario,
-    })
+    });
   }
 
-  render(){
-    return(
-      <div className="center">
-        <form className="pure-form pure-form-stacked"> 
-          <Label 
-            htmlFor='nome' 
-            texto='Quem é você?' 
-            valorInvalido = {this.state.validacao.nomeInvalido}
-          />
-          <Input 
-            id='nome' 
-            placeholder='Nome' 
-            defaultValue={this.state.usuario.nome}
-            maxLength='50'
-            readOnly={false}
+  atualizarGenero(e, genero) {
+    e.preventDefault();
+    let usuario = this.state.usuario;
+    usuario.genero = genero;
+    this.setState({
+      usuario: usuario,
+    });
+  }
 
+  render() {
+    return (
+      <div className="center">
+        <form className="pure-form pure-form-stacked">
+          <Label
+            htmlFor="nome"
+            texto="Quem é você?"
+            valorInvalido={this.state.validacao.nomeInvalido}
+          />
+          <Input
+            id="nome"
+            placeholder="Nome"
+            defaultValue={this.state.usuario.nome}
+            maxLength="50"
+            readOnly={false}
             valorInvalido={this.state.validacao.nomeInvalido}
             onChange={this.atualizarNome.bind(this)}
-
           />
 
-          
+          <Label 
+            texto="Seu gênero:"
+            valorInvalido = {this.state.validacao.generoInvalido}
+          />
+          <GenderSelector 
+            genero={this.state.usuario.genero}
+            atualizarGenero = {this.atualizarGenero.bind(this)}
+            valorInvalido = {this.state.validacao.generoInvalido}
+          />
         </form>
       </div>
-    )
+    );
   }
 }
 
