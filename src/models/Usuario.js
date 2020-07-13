7868bb33ec6f05ac3105377d4c003acc04ab94e7
Brian_Izaki <bkizaki@outlook.com>
2020-07-13 18:39:44 -0300

    add métodos utilizando o Repository

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,4 +1,6 @@
 import Avatar from "./Avatar";
+import Repository from "../infrastructure/Repository";
+const repository = new Repository();
 
 class Usuario {
   constructor(){
@@ -22,6 +24,32 @@ class Usuario {
     return ['m', 'f'].some(param => {
       return this.genero === param;
     });
+  }
+
+  salvar(callback){
+    repository.salvar(this, callback);
+  }
+
+  // o primeiro parametro é uma callback q executa na classe Repository
+  // lá busca os dados do local storage
+  // se tiver executa primeiro parametro
+  // caso contrario executa falha
+  // obs: a função sucesso irá enviar um usuário instanciado, que poderá ser consumedo igual à arrow function desse método.
+  static obter(sucesso, falha){
+    repository.obter(json => {
+      let usuario = new Usuario();
+      usuario.nome = json.nome;
+      usuario.genero = json.genero;
+      usuario.avatar = new Avatar(
+        json.avatar.index, 
+        json.avatar.descricao
+      );
+      sucesso(usuario)
+    }, falha)
+  }
+
+  toString(){
+    return `${this.nome}, ${this.avatar.toString()}`
   }
 };
 
